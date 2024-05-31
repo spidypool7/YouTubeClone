5857b0948dec1406ffa3e46b89503e579bcbdda7
Shubham Suresh Pawar <shubhampawar6291@gmail.com>
2024-05-31 21:14:42 +0530

    youtube clone folder
    YT clone folder added

---
+++ Youtube Clone/README.md
@@ -0,0 +1,7 @@
+# Build and Deploy a Modern YouTube Clone Application in React JS with Material UI 5
+
+ 
+
+ 
+
+### DEPLOYMENT LINK - https://64173e8e65f5505d0c20424d--animated-maamoul-0e5306.netlify.app
---
+++ Youtube Clone/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center", textDecoration: 'none' }}>
+      {logo}
+      <span style={{ color: '#fff', marginLeft: '5px' }}>YouTube</span>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
